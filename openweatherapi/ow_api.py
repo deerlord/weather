66bdb900dd369ef819b061c2f02fc695cb049b2d
Trent Dierking <deerxlord@gmail.com>
2020-12-14 22:27:40 -0600

    one call updates, added icon

--- openweatherapi/ow_api.py
+++ openweatherapi/ow_api.py
@@ -1,4 +1,5 @@
 import aiohttp  # type: ignore
+import aiofiles
 import logging
 from dataclasses import dataclass
 from openweatherapi import models, exceptions
@@ -10,9 +11,20 @@ class OpenWeatherAPI():
     lat: float
     lon: float
     version: str = '2.5'
+    __units: str = 'imperial'
 
     def __post_init__(self):
         self._base_url = f'https://api.openwather.org/data/{self.version}'
+
+    @property
+    def units(self):
+        return self.__units
+
+    @units.setter
+    def units(self, value):
+        if value.lower() in ['standard', 'metric', 'imperial']:
+            self.__units = value.lower()
+
 
     def _url_formatter(self, url: str) -> str:
         url = url[1:] if url.startswith('/') else url
@@ -31,10 +43,11 @@ class OpenWeatherAPI():
     async def _api_request(
         self,
         url: str,
-        params: dict = {}
+        params: dict = {},
     ) -> dict:
         result = {}
         params['appid'] = self.api_key
+        params['units'] = self.__units
         async with aiohttp.ClientSession() as session:
             async with session.get(
                 self._url_formatter(url),
@@ -49,8 +62,7 @@ class OpenWeatherAPI():
 
     async def one_call(self) -> models.OneCallAPIResponse:
         """
-        params:
-          exclude: any subset of {'minutely', 'hourly', 'daily'}
+        Returns a full response for the One Call endpoint
 
         Raises openweatherapi.exceptions.ResponseMalformed
         """
@@ -69,3 +81,11 @@ class OpenWeatherAPI():
             raise exceptions.ResponseMalformed()
         return response
 
+    async def icon(self, icon_id: str):
+        result = None
+        url = f'http://openweathermap.org/img/{icon_id}@2x.png'
+        async with aiohttp.ClientSession() as session:
+            async with session.get(url) as resp:
+                if resp.status == 200:
+                    result = await resp.read()
+        return result
