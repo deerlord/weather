e95b1ac6998a4fea97043ea6f760d4a035646c92
Trent Dierking <deerxlord@gmail.com>
2020-12-15 17:39:17 -0600

    full test coverage

--- tests/test_openweatherapi.py
+++ tests/test_openweatherapi.py
@@ -4,7 +4,7 @@ import unittest
 
 from aioresponses import aioresponses as responses
 
-from openweatherapi import api, models
+from openweatherapi import api, models, exceptions
 from tests.fixtures import openweatherapi as fixtures
 
 
@@ -27,6 +27,20 @@ class TestAPI(unittest.TestCase):
     def tearDown(self):
         pass
 
+    def test_api_request_error(self):
+        with responses() as resps:
+            resps.get(
+                "https://api.openweathermap.org/data/2.5/?units=imperial",
+                payload={},
+                status=500
+            )
+            result = asyncio.run(self.client._api_request(url=''))
+        self.assertDictEqual(result, {})
+
+    def test_units(self):
+        self.client.units = 'METRIC'
+        self.assertEqual(self.client.units, 'metric')
+
     def test_one_call(self):
         with responses() as resps:
             resps.get(
@@ -37,3 +51,35 @@ class TestAPI(unittest.TestCase):
             )
             result = asyncio.run(self.client.one_call())
         self.assertDictEqual(result.dict(), fixtures.ONE_CALL_API_AS_DICT)
+
+    def test_one_call_response_malformed(self):
+        with responses() as resps:
+            resps.get(
+                "https://api.openweathermap.org/data/2.5/"
+                "onecall?lat=0.0&lon=0.0&units=imperial",
+                payload={},
+                status=200
+            )
+            with self.assertRaises(exceptions.ResponseMalformed):
+                asyncio.run(self.client.one_call())
+
+    def test_icon(self):
+        with responses() as resps:
+            resps.get(
+                "http://openweathermap.org/img/wn/01d@2x.png",
+                body=fixtures.ICON_BINARY,
+                status=200
+            )
+            result = asyncio.run(self.client.icon('01d'))
+        self.assertEqual(result, fixtures.ICON_BINARY)
+
+    def test_icon_error(self):
+        with responses() as resps:
+            resps.get(
+                "http://openweathermap.org/img/wn/01d@2x.png",
+                body=fixtures.ICON_BINARY,
+                status=404
+            )
+            with self.assertRaises(exceptions.IconNotFound):
+                asyncio.run(self.client.icon('01d'))
+
