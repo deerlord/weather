085f6c27d074430f6a15f24d7f77070d5957bd82
Trent Dierking <deerxlord@gmail.com>
2020-12-28 23:31:31 -0600

    endpoint cleanup

--- backend/main.py
+++ backend/main.py
@@ -3,33 +3,18 @@ from starlette.responses import StreamingResponse  # type: ignore
 
 from backend import clients
 from backend.models import Measurement
-from openweatherapi import models
 
 app = FastAPI()
 
 
-@app.get("/weather/data", response_model=models.OneCallAPIResponse)
-async def weather_data():
-    result = await clients.openweather().one_call()
-    return result.dict()
-
-
 @app.get("/weather/icon/{icon_id}")
 async def icon(icon_id: str):
-    client = clients.openweather()
-    result = await client.icon(icon_id)
+    result = await clients.openweather().icon(icon_id)
     return StreamingResponse(result, media_type="image/png")
 
 
-@app.get("/weather/current/")
-async def current(time: int, delta: int):
-    client = clients.influxdb()
-    query = f"select * from current where (time >= {time}) and (time <= (time + delta))"
-    return client.query(query)
-
-
 @app.get("/weather/{measurement}/")
-async def hourly(time: int, delta: int, measurement: Measurement):
+async def weather(time: int, delta: int, measurement: Measurement):
     query = (
         "select * from {measurement}"
         f"where (time >= {time}) and (time <= ({time} + {delta}))"
