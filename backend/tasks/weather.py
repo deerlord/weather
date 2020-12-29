5844c9ce6f6fbd3e4837805ad7e5fb8824bbe21b
Trent Dierking <deerxlord@gmail.com>
2020-12-28 23:10:28 -0600

    re-worked weather tasks

--- backend/tasks/weather.py
+++ backend/tasks/weather.py
@@ -1,50 +1,27 @@
 from typing import List
 
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
 from backend.clients import influxdb, openweather
 from openweatherapi import models
 
-
-class TableData(BaseModel):
-    measurement: str
-    time: int
-    fields: dict
+from typing import List, Dict
 
 
-async def munge_one_call(data: models.OneCallAPIResponse):
-    current = [munge(measurement="current", data=data.current.flatten())]
-    minutely = [
-        munge(measurement="minutely", data=minutely.flatten())
-        for minutely in data.minutely
-    ]
-    hourly = [
-        munge(measurement="hourly", data=hourly.flatten()) for hourly in data.hourly
-    ]
-    daily = [munge(measurement="daily", data=daily.flatten()) for daily in data.daily]
-    return (current, minutely, hourly, daily)
-
-
-def munge(measurement: str, data: dict):
-    data.pop("weather", [])
-    return {"measurement": measurement, "time": data.pop("dt"), "fields": data}
-
-
-async def write_points(points: List[TableData]):
-    for point in points:
-        influxdb().write_points(point.dict())
-
-
-async def create_points(measurement: str, data: List[BaseModel]):
+def weather_points(measurement: str, data: List[models.WeatherDataBaseModel]):
     for point in data:
-        yield TableData(measurement=measurement, time=point.dt, fields=point.dict())
+        yield {
+            'measurement': measurement,
+            'time': point.dt,
+            'fields': point.dict()
+        }
 
 
-async def periodic_weather():
+async def weather_data():
     data = await openweather().one_call()
-    current_points = create_points(measurement="current", data=[data.current])
-    hourly_points = create_points(measurement="hourly", data=data.hourly)
-    minutely_points = create_points(measurement="minutely", data=data.minutely)
-    daily_points = create_points(measurement="daily", data=data.minutely)
+    current_points = weather_points(measurement="current", data=[data.current])
+    hourly_points = weather_points(measurement="hourly", data=data.hourly)
+    minutely_points = weather_points(measurement="minutely", data=data.minutely)
+    daily_points = weather_points(measurement="daily", data=data.minutely)
     for points in [current_points, hourly_points, minutely_points, daily_points]:
-        write_points(points=points)
+        influxdb().write_points(points=points)
