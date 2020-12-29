9661f52ab63ae2798078f5cdc5883534f7a748ea
Trent Dierking <deerxlord@gmail.com>
2020-12-28 22:14:50 -0600

    weather database tasks

--- backend/tasks/weather.py
+++ backend/tasks/weather.py
@@ -1,14 +1,15 @@
+from typing import List
+
+from pydantic import BaseModel
+
 from backend.clients import influxdb, openweather
 from openweatherapi import models
-from pydantic import BaseModel
-from typing import List
 
 
 class TableData(BaseModel):
     measurement: str
     time: int
     fields: dict
-
 
 
 async def munge_one_call(data: models.OneCallAPIResponse):
@@ -36,31 +37,14 @@ async def write_points(points: List[TableData]):
 
 async def create_points(measurement: str, data: List[BaseModel]):
     for point in data:
-        yield TableData(
-            measurement=measurement,
-            time=point.dt,
-            fields=point.dict()
-        )
+        yield TableData(measurement=measurement, time=point.dt, fields=point.dict())
 
 
 async def periodic_weather():
     data = await openweather().one_call()
-    current_points = create_points(
-        measurement='current',
-        data=[data.current]
-    )]
-    hourly_points = create_points(
-        measurement='hourly',
-        data=data.hourly
-    )
-    minutely_points = create_points(
-        measurement='minutely',
-        data=data.minutely
-    )
-    daily_points = create_points(
-        measurement='daily',
-        data=data.minutely
-    )
+    current_points = create_points(measurement="current", data=[data.current])
+    hourly_points = create_points(measurement="hourly", data=data.hourly)
+    minutely_points = create_points(measurement="minutely", data=data.minutely)
+    daily_points = create_points(measurement="daily", data=data.minutely)
     for points in [current_points, hourly_points, minutely_points, daily_points]:
         write_points(points=points)
-
