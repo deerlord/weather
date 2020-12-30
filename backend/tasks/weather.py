0896946af8dac86dbc2da5cc0a90b023166a56c5
Trent Dierking <deerxlord@gmail.com>
2020-12-29 22:18:37 -0600

    added ability to gather weather data and add to influxdb; needs tests

--- backend/tasks/weather.py
+++ backend/tasks/weather.py
@@ -1,14 +1,11 @@
 from copy import deepcopy
-from typing import List, Callable
+from typing import Callable, List
 
 from backend.clients import influxdb, openweather
 from openweatherapi import models
 
 
-def weather_points(
-    data: List[models.WeatherDataBaseModel],
-    munge: Callable
-):
+def weather_points(data: List[models.WeatherDataBaseModel], munge: Callable):
     for each in data:
         dt, munged = munge(data=each)
         for measurement, fields in munged.items():
@@ -31,61 +28,42 @@ def munge_weather_data(data: models.WeatherDataBaseModel) -> tuple:
     )
     """
     # get name from model
-    measurement = data.__name__.lower()
+    measurement = type(data).__name__.lower()
     # deep copy so we dont mess up the mode
     data = deepcopy(data.dict())
     # find fields that area dicts
-    flatten_keys = [
-        field
-        for field, value in data.items()
-        if isinstance(value, dict)
-    ]
+    flatten_keys = [field for field, value in data.items() if isinstance(value, dict)]
     # create {measurement: fields} map
-    retval = {
-        f'{measurement}_{field}': data.pop(field)
-        for field in flatten_keys
-    }
+    retval = {f"{measurement}_{field}": data.pop(field) for field in flatten_keys}
     # check for weather
-    weather = data.pop('weather', {})
+    weather = data.pop("weather", {})
     # get weather IDs and serialize them
     if weather:
-        ids = [
-            each['id']
-            for each in weather
-        ]
-        retval.update({'weather': str(ids)})
+        ids = [each["id"] for each in weather]
+        retval.update({f"{measurement}_weather": {"ids": str(ids)}})
     # include rest of model data (int/float/str fields)
     retval.update({measurement: data})
     # include 'dt' value once
-    return (data.pop('dt'), retval)
+    return (data.pop("dt"), retval)
 
 
 def munge_alert_data(data: models.Alert) -> tuple:
     data = deepcopy(data.dict())
-    return (data['start'], {'alert': data})
+    return (data["start"], {"alert": data})
 
 
 async def weather_data():
     data = await openweather().one_call()
-    current_points = weather_points(
-        data=[data.current],
-        munge=munge_weather_data
-    )
-    hourly_points = weather_points(
-        data=data.hourly,
-        munge=munge_weather_data
-    )
-    minutely_points = weather_points(
-        data=data.minutely,
-        munge=munge_weather_data
-    )
-    daily_points = weather_points(
-        data=data.minutely,
-        munge=munge_weather_data
-    )
-    alert_points = weather_points(
-        data=data.alerts,
-        munge=munge_alert_data
-    )
-    for points in [current_points, hourly_points, minutely_points, daily_points, alert_points]:
+    current_points = weather_points(data=[data.current], munge=munge_weather_data)
+    hourly_points = weather_points(data=data.hourly, munge=munge_weather_data)
+    minutely_points = weather_points(data=data.minutely, munge=munge_weather_data)
+    daily_points = weather_points(data=data.minutely, munge=munge_weather_data)
+    alert_points = weather_points(data=data.alerts, munge=munge_alert_data)
+    for points in [
+        current_points,
+        hourly_points,
+        minutely_points,
+        daily_points,
+        alert_points,
+    ]:
         influxdb().write_points(points=points)
