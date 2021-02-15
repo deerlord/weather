ee0dfc9845e9c9eab418868d601aa755ac0d2578
Trent Dierking <deerxlord@gmail.com>
2021-02-15 05:57:22 +0000

    updated weather router

--- backend/server.py
+++ backend/server.py
@@ -1,44 +1,23 @@
 from typing import Any, Dict
 
-from fastapi import FastAPI, HTTPException
+from fastapi import APIRouter, FastAPI, HTTPException
 from openweathermap import api  # type: ignore
 from starlette.responses import StreamingResponse  # type: ignore
 
 from backend import cache, clients, settings
 from backend.models import weather as models
 
+router = APIRouter()
 app = FastAPI()
 
 
-@app.get("/weather/icon/{icon_id}")
+@router.get("/icon/{icon_id}")
 async def icon(icon_id: str):
     result = await api.icon(icon_id)
     return StreamingResponse(result, media_type="image/png")
 
 
-# needs caching around endpoint?
-@app.get("/weather/widget/old", response_model=models.WidgetOverviewOld)
-async def current(lat: float, lon: float):
-    """
-    see https://openweathermap.org/widgets-constructor for example
-    """
-    client = api.OpenWeatherData(appid=settings.OPEN_WEATHER_MAP_API_KEY)
-    data = await client.one_call(
-        lat=lat, lon=lon, units=settings.OPEN_WEATHER_MAP_UNITS
-    )
-    air_pollution_forecast = await client.air_pollution_forecast(lat=lat, lon=lon)
-    uvi_forecast = await client.uvi_forecast(lat=lat, lon=lon, cnt=8)
-    location = await cache.location(lat=lat, lon=lon)
-    result = {
-        "data": data,
-        "air_pollution_forecast": air_pollution_forecast.list,
-        "uvi_forecast": uvi_forecast,
-        "location": location,
-    }  # type: Dict[str, Any]
-    return result
-
-
-@app.get("/weather/map/{layer}")
+@router.get("/map/{layer}")
 async def map(layer: models.MapLayer, lat: float, lon: float, zoom: int):
     client = api.OpenWeatherMap(appid=settings.OPEN_WEATHER_MAP_API_KEY)
     method = getattr(client, layer)
@@ -49,7 +28,7 @@ async def map(layer: models.MapLayer, lat: float, lon: float, zoom: int):
     return StreamingResponse(result, media_type="image/png")
 
 
-@app.get("/weather/widget/overview", response_model=models.WidgetResponseModel)
+@router.get("/widget/overview", response_model=models.WidgetResponseModel)
 async def widget_overview(lat: float, lon: float):
     client = api.OpenWeatherData(appid=settings.OPEN_WEATHER_MAP_API_KEY)
     one_call = await client.one_call(
@@ -74,3 +53,6 @@ async def widget_overview(lat: float, lon: float):
         },
     }
     return result
+
+
+app.include_router(router, prefix="/weather")
