a299496d8b85a804bc4e46479257deea6329c495
Trent Dierking <deerxlord@gmail.com>
2021-02-15 06:04:11 +0000

    removed old clients

--- backend/clients.py
+++ backend/clients.py
@@ -2,16 +2,6 @@ from os import environ as ENV
 
 from influxdb import InfluxDBClient  # type: ignore
 
-from openweatherapi.api import OpenWeatherAPI
-
-
-def openweather():
-    return OpenWeatherAPI(
-        api_key=str(ENV["openweather_api_key"]),
-        lat=float(ENV["latitude"]),
-        lon=float(ENV["longitude"]),
-    )
-
 
 def influxdb():
     return InfluxDBClient(
